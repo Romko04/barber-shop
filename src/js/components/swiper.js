2d04c4ad06f2f4f3fc1f979f7ee28f8dbeaeeef0
Roman <kurpelroma@gmail.com>
2023-04-15 13:06:16 +0300

    Закінчив swiper

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,10 +1,19 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+Swiper.use([Navigation, Pagination, Scrollbar]);
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-btn-next',
     prevEl: '.swiper-btn-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4
+    }
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true
   },
 });
